2b16ab882d2abdff6884b00298eb1f9b1d856988
root <root@MI-wsf>
2024-07-13 20:02:11 +0800

    Initial commit

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ DOTFILES_DIR=~/dotfiles
 
 # Create symbolic links
 ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
 
 # Source the .bashrc to apply changes
 source ~/.bashrc
